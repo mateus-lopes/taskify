079bbc36ac008fb519ddcdcd7ccc387263d7c0de
Mateus Lopes Albano <mateusalbano22@gmail.com>
2022-06-23 10:57:57 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>{{title}}</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      title: 'hello world'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
