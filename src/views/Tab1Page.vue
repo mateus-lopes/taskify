2a3ea991a024cd69b641e5814a87324fc4151130
mateus-lopes <mateusalbano22@gmail.com>
2024-05-22 01:56:30 -0300

    feat: :sparkles: Modal to choose user image

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -5,9 +5,10 @@
         <ion-text class="title">My tasks</ion-text>
       </ion-col>
       <ion-col size="2">
-        <ion-avatar>
-          <img alt="Silhouette of a person's head" src="https://avatars.githubusercontent.com/u/50590861?v=4?s=400" />
-        </ion-avatar>
+        <ion-avatar @click.stop="goProfile()" >
+        <img v-if="user.photoUrl != ''" :alt="user.name" :src="user.photoUrl" />
+        <img v-else :alt="user.name" src="https://practice365.co.uk/a82023/wp-content/uploads/sites/1113/2023/05/Default-Profile-Picture-Transparent.png" />
+      </ion-avatar>
       </ion-col>
     </ion-row>
     <ion-content class="ion-padding-top">
@@ -23,10 +24,25 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonSegment, IonSegmentButton, IonLabel, IonGrid, IonRow, IonCol, IonText } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
 import { apps, checkmarkDoneOutline, checkmarkOutline
  } from 'ionicons/icons';
+
+const router = useRouter()
+
+const user = ref({
+  name: 'Full Name',
+  email: 'email',
+  // photoUrl: 'https://avatars.githubusercontent.com/u/50590861?v=4?s=400'
+  photoUrl: ''
+})
+
+const goProfile = () => {
+  router.push('/tabs/tab3')
+}
 
 </script>
 
--- src/views/Tab3Page.vue
+++ src/views/Tab3Page.vue
@@ -1,28 +1,90 @@
 <template>
   <ion-page class="page">
-    <ion-row class="ion-justify-content-center">
-        <ion-avatar>
-          <img alt="Silhouette of a person's head" src="https://avatars.githubusercontent.com/u/50590861?v=4?s=400" />
-        </ion-avatar>
+    <ion-row class="ion-justify-content-center ion-padding-top">
+      <ion-avatar @click.stop="isOpen = true" >
+        <img v-if="user.photoUrl != ''" :alt="user.name" :src="user.photoUrl" />
+        <img v-else :alt="user.name" src="https://practice365.co.uk/a82023/wp-content/uploads/sites/1113/2023/05/Default-Profile-Picture-Transparent.png" />
+      </ion-avatar>
+      <ion-modal :is-open="isOpen" :fullscreen="true">
+        <ion-header>
+          <ion-toolbar>
+            <ion-buttons slot="start">
+              <ion-button @click="isOpen = false">Cancel</ion-button>
+            </ion-buttons>
+            <ion-title
+              class="ion-text-center"
+            >
+              Profile Picture
+            </ion-title>
+            <ion-buttons slot="end">
+              <ion-button :strong="true">Confirm</ion-button>
+            </ion-buttons>
+          </ion-toolbar>
+        </ion-header> 
+        <ion-content>
+          <ion-title
+            class="ion-text-center ion-margin-top ion-padding"
+          >
+            <small v-if="user.photoUrl === ''">
+                Take a picture or select one from your gallery
+              </small>
+              <small v-else>
+                {{ user.photoUrl }}
+              </small>
+            </ion-title>
+            <ion-grid>
+              <ion-row class="ion-justify-content-center">
+                <ion-button size="large">
+                  <ion-icon slot="icon-only" :icon="camera"></ion-icon>
+                </ion-button>
+                <ion-button size="large" color="medium">
+                  <ion-icon slot="icon-only" :icon="imageOutline"></ion-icon>
+                </ion-button>
+                <ion-button size="large" color="danger">
+                  <ion-icon slot="icon-only" :icon="trashOutline"></ion-icon>
+                </ion-button>
+              </ion-row>
+              <ion-row class="ion-justify-content-center">
+                <!-- example picture -->
+                <ion-grid class="ion-margin ion-padding" >
+                <img v-if="user.photoUrl !== ''" shape="round" :src="user.photoUrl" />
+              </ion-grid>
+            </ion-row>
+            </ion-grid>
+        </ion-content>
+      </ion-modal>
     </ion-row>
     <ion-content>
       <ion-title class="ion-no-padding name">
-        Mateus Lopes Albano
+        {{ user.name }}
       </ion-title>
       <ion-text color="medium email">
-        mateusalbano22@gmail.com
+        {{ user.email }}
       </ion-text>
     </ion-content>
-    <ion-button shape="round" fill="clear" expand="full" color="medium">reset account</ion-button>
+    <ion-button shape="round" fill="clear" expand="full" color="medium">change password</ion-button>
     <ion-button shape="round" fill="clear" expand="full" color="danger">Logout</ion-button>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonSegment, IonSegmentButton, IonLabel, IonGrid, IonRow, IonCol, IonText } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
-import { apps } from 'ionicons/icons';
-import { computed } from 'vue';
+import { ref } from 'vue';
+import { camera, imageOutline, trashOutline } from 'ionicons/icons';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonSegment, IonSegmentButton, IonLabel, IonGrid, IonRow, IonCol, IonText, IonAvatar, IonModal, IonButton, IonIcon } from '@ionic/vue';
+
+const isOpen = ref(false);
+
+const cancel = () => {
+  isOpen.value = false;
+}
+
+const user = ref({
+  name: 'Full Name',
+  email: 'email',
+  // photoUrl: 'https://avatars.githubusercontent.com/u/50590861?v=4?s=400'
+  photoUrl: ''
+})
+
 </script>
 
 <style scoped>
